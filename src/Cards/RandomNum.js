03bcc38da7b5e0d97b9c4c20ae21214cc3e79b94
armani <razzarioa@gmail.com>
2019-01-29 22:42:35 +0600

    Added component RandomNum.js with basic styles, modified App cont and styles

---
+++ src/Cards/RandomNum.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './RandomNum.css';
+
+
+class Cards extends Component {
+    render() {
+        return(
+            <div className="card">
+                <div className="circle">
+                    <p className="number">40</p>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Cards;
+
+
+
+// Create a component with cards and numbers
+// Add basic styles for our cards
+// Add button for click with event
+// Define constructor or default properties for numbers
+// Define a method that could generate numbers
+// Try to do it with state inside Cards component
+
+
+
+
